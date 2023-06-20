f767591938a6fa1526f0a4613d2e60fe17b0c5d7
Chris Jenkins <chrisj@redhat.com>
2023-06-20 12:02:29 +0200

    Dummy secrets added

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
 
 ## Test Values for Secret Scanning
+aws_secret_access_key="ASDASDASDASD";
+azure_function_key="QWEQWEQWEQWEQWE";
 myUsername="QWEQWEQWEQWEQWE";
 myPassword="qweqweqweqweqweqwe";
 myToken="WQEQWEqweqweQWEQWEqweqwe";
